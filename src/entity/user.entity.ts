77cdd88ff6aac988027d71fed70c523db5d15a4d
Ruangwit Sanguanpong <ruangwit.s@TorThuys-MacBook-Pro.local>
2020-05-14 17:08:32 +0700

    [+] change column name in db and status

--- src/entity/user.entity.ts
+++ src/entity/user.entity.ts
@@ -1,22 +1,32 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm'
+import { UserStatus } from '~/constant'
 
 @Entity('user')
 export class UserEntity {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid', { name: 'user_id' })
   userId: number
 
-  @Column('varchar', { length: 50 })
+  @Column({ type: 'varchar', name: 'username', length: 50 })
   username: string
 
-  @Column('varchar', { length: 50 })
+  @Column({ type: 'varchar', name: 'password', length: 50 })
   password: string
 
-  @Column('varchar', { length: 50 })
+  @Column({ type: 'varchar', name: 'first_name', length: 50 })
   firstname: string
 
-  @Column('varchar', { length: 50 })
+  @Column({ type: 'varchar', name: 'last_name', length: 50 })
   lastname: string
 
-  @Column('varchar', { length: 50 })
+  @Column({ type: 'varchar', name: 'display_name', length: 50 })
   displayname: string
+
+  @Column({
+    type: 'enum',
+    enum: UserStatus,
+    default: UserStatus.ACTIVE,
+    name: 'status',
+    lenght: 10,
+  })
+  status: boolean
 }
